acbdd72af90acdbdda462ecbed57a1a8b1fd721e
samgan-khan <mohdsamgankhan@gmail.com>
2018-11-24 11:33:19 +0530

    added landing page

--- Views/Pages/home.php
+++ Views/Pages/home.php
@@ -1,1 +1,55 @@
-<h1>Samgan Rocks</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mohd. Samgan Khan">
+
+    <title>Febie - Framework</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/Resources/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+    <!-- Custom styles for this template -->
+    <link href="/Resources/css/main.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<!-- Page Header -->
+<header class="masthead" style="background-image: url('/Resources/img/home-bg.jpg'); padding-top: 80px;">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+                <div class="site-heading">
+                    <h1>Febie</h1>
+                    <span class="subheading">Simple yet elegant MVC framework.</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<!-- Footer -->
+<footer style="padding: 0px; margin-top: -60px;">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+                <p class="copyright text-muted">Copyright &copy; febie <?= date('Y') ?></p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+</body>
+
+</html>
